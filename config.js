63e7b6c1db82071a2e80e24613baa31c45b112a7
sangdph <sangdph.developer@gmail.com>
2022-07-17 23:56:17 +0700

    build

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cục cưng à!',
+    introDesc: `Anh nhớ em quá à 🥺🥺🥺 `,
+    btnIntro: 'Cho anh hôn miếng nha 😛',
+    title: 'Ờ thì cũng lâu rồi mình chưa gặp nhau á, anh muốn ôm em quá à ',
+    desc: 'Lát mình đi uống cafe ngắm hoàng hôn ha em?',
+    btnYes: 'Triển luôn anh yêu ơi 😎',
+    btnNo: 'Hong, đi gì mà đi hoài em phải làm diệc 🥺🥺🥺',
+    question:'Em thấy anh có đáng iu hong nè 🙈?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Anh quá là tuyệt dời luôn. Quá là sến nha, nhưng em thích ',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Lát 5h anh qua đón cục cưng nha',
+    btnAccept: 'Okie anh iu nè <3 ',
+    messLink: 'https://www.facebook.com/sangdph' 
+}
